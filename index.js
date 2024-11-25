4e53e66693bba55372054b9c6761709b7f287995
LG125YT <lg125yt@gmail.com>
2024-11-25 14:11:23 -0500

    add my page

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
-const fs = require('fs');
 
 const app = express();
 
 function absolute(local) {
-    return `/home/lg125yt/CyberWard/${local}`;
+    return `/home/lg125yt/CyberWard/public/${local}`;
 }
 
+
 app.use("/", (req, res) => {
-    res.sendFile(absolute('public/index.html'));
-    return;
+    let path = req.path;
+
+    if (path === "/") {
+        res.sendFile(absolute('index.html'));
+        return;
+    } else {
+        if (path.startsWith("//")) {
+            path = path.replace("^\/\/","/")
+        }
+
+        //Issue: You cannot go to https://cyberward.xyz/LG125YT without the css not being fetched (If you append a forward slash to the end of the url, it works fine). This is the solution.
+        //It feels like a hack, but I can't find any other way to fix this.
+        if (path === "/LG125YT") {
+            res.sendFile(absolute('LG125YT/index.html'))
+        }
+
+        res.sendFile(absolute(path));
+        return;
+    }
 })
+
 
 app.set('trust proxy', '127.0.0.1')
 
